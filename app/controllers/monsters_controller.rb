ee10fd668ba311b4c9da3c68b3ec9ff734918bc1
System Administrator <root@alexs-MacBook-Air.local>
2018-07-27 11:33:45 -0700

    sure

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -14,20 +14,26 @@ class MonstersController < ApplicationController
 
 
 	def monster_params
-		params.require(:monster).permit(:active)
+		params.require(:monster).permit(:active,:name,:level)
 	end
 
 
 
 	def create
 		
-				@monster = Monster.new(monster_set)
+			@monster = Monster.new(monster_params)
 
-			render json: @monster
+			@monster.save
+			   
+
+			render json: @monster, status: 201
+
 		end
 
-
-		def monster_set
-			prams.require(:monster).permit(:name,:level)
+		def destroy
+			@monster = Monster.destroy(params[:id])
+			@monster.save
+			render json: @monster, status: 410
 		end
+		
 end
--- db/migrate/20180727040449_create_monsters_table.rb
+++ db/migrate/20180727040449_create_monsters_table.rb
@@ -2,7 +2,7 @@ class CreateMonstersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :monsters do |t|
     	t.string :name
-    	t.integer :level
+    	t.integer :level1
     end
   end
 end
