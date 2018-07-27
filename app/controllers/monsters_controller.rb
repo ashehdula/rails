c9971e3fd727fefbd8d1dd78e5c56a7b3cd94299
System Administrator <root@alexs-MacBook-Air.local>
2018-07-27 08:20:37 -0700

    firsst

---
+++ app/controllers/monsters_controller.rb
@@ -0,0 +1,33 @@
+class MonstersController < ApplicationController
+	def index
+		render json: Monster.all
+	end
+
+
+
+
+	def update
+		@monster = Monster.find(params[:id])
+		@monster.update_attributes(monster_params)
+		render json: @monster
+	end
+
+
+	def monster_params
+		params.require(:monster).permit(:active)
+	end
+
+
+
+	def create
+		
+				@monster = Monster.new(monster_set)
+
+			render json: @monster
+		end
+
+
+		def monster_set
+			prams.require(:monster).permit(:name,:level)
+		end
+end
